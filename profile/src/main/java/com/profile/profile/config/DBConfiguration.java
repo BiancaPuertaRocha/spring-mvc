a0861a9cfd05add17e07074779cf098dcecac701
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-05-03 09:14:41 -0300

    profiles dev and prod working

--- profile/src/main/java/com/profile/profile/config/DBConfiguration.java
+++ profile/src/main/java/com/profile/profile/config/DBConfiguration.java
@@ -1,5 +1,6 @@
 package com.profile.profile.config;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +32,35 @@ public class DBConfiguration {
         return "mysql";
     }
 
+    public String getDriverClassName() {
+        return driverClassName;
+    }
 
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
 
+    public String getUrl() {
+        return url;
+    }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
